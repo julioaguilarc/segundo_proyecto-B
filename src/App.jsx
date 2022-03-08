551da70ca68bd89539e83168d909190a49e39249
Julio Aguilar Carmona <julio.ac@ittux.edu.mx>
2022-03-08 16:51:29 -0600

    Ejemplos de manejo de variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+// ECMAscript 5
+var textoVar = "texto var 1";
+
 // ECMAscript 6
+let textoLet = "texto let 1";
+const textoConst = "texto const 1";
+
+textoVar = "texto var 2";
+textoLet = "texto let 2";
+// textoConst = "texto const 2";
+
+var textoVar = "texto var 3";
+// let textoLet = "texto let 3";
+// const textoConst = "texto const 3";
+
+if (true) {
+  var textoVar = 5;
+  let textoLet = 10;
+  // textoConst = 15;
+}
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Segundo Proyecto</h1>
+      <h1>{textoVar}</h1>
+      <h1>{textoLet}</h1>
+      <h1>{textoConst}</h1>
     </div>
   );
 };
